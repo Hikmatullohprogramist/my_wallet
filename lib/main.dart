02ff79f2b05ca773fc0e30d2e4d34184f22b3d41
Hikmatulloh <96409206+Hikmatullohprogramist@users.noreply.github.com>
2023-06-26 14:46:46 +0500

    kommited

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_wallet/pages/home_page.dart';
+import 'package:my_wallet/pages/splash/splash_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      home: const SplashPage(),
     );
   }
 }
